67eee2126496509715b238cbf8f43ec743ed2060
SeungBin-li <95731331+SeungBin-li@users.noreply.github.com>
2023-02-21 20:10:13 +0900

    Add files via upload
    day3 report

---
+++ Exercise6_21.java
@@ -0,0 +1,14 @@
+class Exercise6_21 {
+
+    public static int abs(int value) {
+        return Math.abs(value);
+    }
+
+    public static void main(String[] args) {
+        int value = 5;
+        System.out.println(value + "의 절대값 :" + abs(value));
+        value = -10;
+        System.out.println(value + "의 절대값 :" + abs(value));
+    }
+}
+//예상 결과 : 5의 절대값 : 5 / -10의 절대값 : 10
